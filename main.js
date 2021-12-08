00d7f5d00eb4b596e7e2070784080d76593eafa8
alesia-k <aliciaJC@yandex.ru>
2021-12-08 20:25:27 +0200

    burger-menu

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let mainMenuToggle = document.querySelector(".main-menu__toggle");
+let mainMenu = document.querySelector(".main-menu");
 
 mainMenuToggle.addEventListener('click', function() {
     mainMenuToggle.classList.toggle("active");
+    mainMenu.classList.toggle("active");
 });
+
