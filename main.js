0ae9dd756848b67d85653c08873c30bdb6cc705f
alesia-k <aliciaJC@yandex.ru>
2021-12-29 18:53:16 +0200

    button "up", smooth scroll

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 let mainMenuToggle = document.querySelector(".main-menu__toggle");
 let mainMenu = document.querySelector(".main-menu");
 
-mainMenuToggle.addEventListener('click', function() {
+mainMenuToggle.addEventListener('click', function () {
     mainMenuToggle.classList.toggle("active");
     mainMenu.classList.toggle("active");
 });
+
+
+/* scroll */
+
+window.onload = () => {
+
+    const up = document.querySelector(".up-button");
+
+    let scrolled,
+        timer;
+
+    window.onscroll = () => {
+        if (window.pageYOffset > 729) {
+            up.classList.add("active");
+        } else (up.classList.remove("active"));
+    };
+
+    onscroll();
+
+
+    up.addEventListener("click", () => {
+        scrolled = window.pageYOffset;
+        scrollToTop();
+    });
+
+    function scrollToTop() {
+        if (scrolled > 0) {
+            window.scrollTo(0, scrolled);
+            scrolled = scrolled - 300;
+            timer = setTimeout(scrollToTop, 100);
+        } else {
+            clearTimeout(timer); {
+                window.scrollTo(0, 0)
+            }
+        }
+    }
+};
+
 
